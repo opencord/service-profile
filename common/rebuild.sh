bc4e07cca11527e1d3ab87032c900a42e6ed6e02
Scott Baker <smbaker@gmail.com>
2016-07-26 23:44:55 -0700

    allow rebuild of single service
    Change-Id: Ia468f81ffe22e9628adb37b3fddbdbe3fb3c8087

--- common/rebuild.sh
+++ common/rebuild.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 display_usage() { 
-    echo -e "\nUsage:\n$0 [xos-listen-port] \n" 
+    echo -e "\nUsage:\n$0 [xos-listen-port] [service-name]\n" 
 } 
 
 if [  $# -lt 1 ] 
@@ -10,11 +10,20 @@ then
     exit 1
 fi 
 
+if [ $# -eq 2 ]; then
+echo "Sending rebuild request to XOS for service $2"
+STATUS=`curl -X POST -F "service=$2" 0.0.0.0:$1/api/utility/onboarding/xos/rebuild/ 2> /dev/null`
+if [[ "$STATUS" != "true" ]]; then
+    echo "Rebuild request failed"
+    exit -1
+fi
+else
 echo "Sending rebuild request to XOS"
 STATUS=`curl -X POST 0.0.0.0:$1/api/utility/onboarding/xos/rebuild/ 2> /dev/null`
 if [[ "$STATUS" != "true" ]]; then
     echo "Rebuild request failed"
     exit -1
+fi
 fi
 
 echo "Rebuild request accepted"
