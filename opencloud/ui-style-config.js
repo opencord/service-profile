bb11ab5420e84c2b8b5c53f4e3bf1fd76b29fe95
Matteo Scandolo <matteo.scandolo@gmail.com>
2017-02-02 12:10:49 -0800

    CORD-772 Added make target and TOSCA spec for example UI extension
    Change-Id: If26ccc840259e82edcec11df2949fd6798bcdfbc

---
+++ opencloud/ui-style-config.js
@@ -0,0 +1,18 @@
+angular.module('app')
+  .constant('StyleConfig', {
+    projectName: 'OpenCloud',
+    favicon: 'opencloud-favicon.png',
+    background: 'opencloud-bg.jpg',
+    payoff: 'Your OS resource manager',
+    logo: 'opencloud-logo.png',
+    routes: [
+        {
+            label: 'Slices',
+            state: 'xos.core.slices'
+        },
+        {
+            label: 'Sites',
+            state: 'xos.core.sites'
+        }
+    ]
+});
