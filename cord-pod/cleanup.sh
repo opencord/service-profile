8043e4b14a6ff310238c70123c21b8bc995494c8
Scott Baker <smbaker@gmail.com>
2017-03-06 15:45:33 -0800

    delete exampleservice network in cleanup script
    Change-Id: I2f1ffb4f8bff18ca19f26e80be8ab3b4d4a5e2c8

--- cord-pod/cleanup.sh
+++ cord-pod/cleanup.sh
@@ -50,3 +50,4 @@ neutron net-delete management || true
 neutron net-delete management_hosts || true
 neutron net-delete mysite_vsg-access || true
 neutron net-delete public || true
+neutron net-delete exampleservice_network || true
